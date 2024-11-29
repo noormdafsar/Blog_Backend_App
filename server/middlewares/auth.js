babf56ed9680a921b7a7e4e1c1d5e8a682e0f1a0
Nooruddin Md Afsar <nooruddin.mdafsar.it25@heritageit.edu.in>
2024-11-29 20:46:23 +0530

    Update auth.js

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -4,7 +4,7 @@ const ErrorHandler = require('../utils/errorHandler');
 
 exports.isAuthenticatedUser = async (req, res, next) => {
   try {
-    const { token } = req.cookies;
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
     if (!token) {
       return next(new ErrorHandler('Please login to access this resource', 401));
